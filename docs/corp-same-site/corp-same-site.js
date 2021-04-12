05e5df69f8df2167b0ab31ad8fcc64dfb7d1b10e
Virac David AUK <auk.david@gmail.com>
2021-04-12 17:57:06 +0200

    Same script with different headers

---
+++ docs/corp-same-site/corp-same-site.js
@@ -0,0 +1,13 @@
+(function() {
+  const self = document.currentScript
+  const url = self.src
+  const filename = url.substring(url.lastIndexOf('/') + 1);
+
+
+  self.parentNode.textContent = `${filename} loaded`
+
+  const sab = new SharedArrayBuffer()
+  
+  const start = window.performance.now()
+  setTimeout(() => console.log('Elapsed time:', window.performance.now() - start), 1000)
+})()
